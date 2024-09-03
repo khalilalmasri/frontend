f7d4e8494024ab17c787124d0db65e17b18eb867
khalilalmasri <96783293+khalilalmasri@users.noreply.github.com>
2024-09-03 15:13:44 +0300

    Add React Toastify for Notifications and Enhance Create Post Form
    Integrate React Toastify for displaying notifications and enhance the Create Post form
    with form validation and file upload functionality. This update includes adding the
    React Toastify library, configuring it in the project, and implementing it within the
    Create Post component to provide real-time feedback on form submissions. Additionally,
    the form now includes fields for title, description, categories, and file upload, with
    validation to ensure all fields are filled out correctly before submission. Styling
    updates are also applied to the Create Post form for better user experience.

--- src/pages/create-post/CreatePost.jsx
+++ src/pages/create-post/CreatePost.jsx
@@ -1,8 +1,68 @@
+import { useState } from "react";
+import "./create-post.css";
+import { toast , ToastContainer } from "react-toastify";
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [categories, setCategories] = useState("");
+  const [file, setFile] = useState(null);
+   // form submission
+  const formSubmitHandler = (e) => {
+    e.preventDefault(); //to unreload the page
+    // if(!title || !description || !categories || !file) return alert("all fields are required");
+    if(title.trim() === "" ) return toast.error("title fields are required");
+    if(description.trim() === "" ) return toast.error("description fields are required");
+    if(categories.trim() === "" ) return toast.error("categories fields are required");
+    if(!file) return toast.error("file  are required");
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("description", description);
+    formData.append("categories", categories);
+    formData.append("image", file);
+    console.log({ title, description, categories, file });
+  };
   return (
-    <div>
-      <h1>CreatePost</h1>
-    </div>
+    <section className="create-post">
+      <ToastContainer theme="dark" position="top-center" />
+      <h1 className="create-post-title">Create New Post</h1>
+      <form className="create-post-form" onSubmit={formSubmitHandler}>
+        <input
+          type="text"
+          placeholder="Post Title"
+          className="create-post-input"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <select
+          className="create-post-input"
+          value={categories}
+          onChange={(e) => setCategories(e.target.value)}
+        >
+          <option disabled value="">
+            select a category
+          </option>
+          <option value="music">music</option>
+          <option value="coffee">coffee</option>
+        </select>
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          className="create-post-textarea"
+          rows="5"
+          placeholder="Post Description"
+        ></textarea>
+        <input
+          type="file"
+          name="file"
+          id="file"
+          className="create-post-upload"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <button type="submit" className="create-post-btn">
+          Create
+        </button>
+      </form>
+    </section>
   );
 };
 
